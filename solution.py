5a377bb1e6640183543a93af3a551a8021282197
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-13 22:06:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import random 
+
+import statsmodels.stats.proportion as st
 
 chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
@@ -11,4 +12,11 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return bool(random.getrandbits(1)) # Ваш ответ, True или False
+    
+    res = st.proportions_ztest([x_success, y_success], [x_cnt, y_cnt], 0)
+    a = 0.08
+    ret = False
+    if res[1] <=a:
+        ret = True
+        
+    return ret # Ваш ответ, True или False
